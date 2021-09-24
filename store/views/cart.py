fc98976a554657451d934481ac71c86e7d85edd7
ankit-khater <ankitkhaterak@gmail.com>
2021-09-24 13:32:42 +0600

    implementing cart using session concept and also implementing order place functionality and their corresponding views

---
+++ store/views/cart.py
@@ -0,0 +1,24 @@
+
+from django.shortcuts import render,redirect,HttpResponse
+
+from store.models import Customer
+from django.contrib.auth.hashers import check_password
+from django.views import View
+from store.models.product import Product
+
+
+
+
+#cart class base concept for handel request
+class Cart(View):
+
+    def get(self,request):
+
+        ids=list(request.session.get('cart').keys())
+        print("---------------",ids)
+        if not ids:
+            ids=[]
+        products=Product.get_products_by_id(ids)
+        print(products)
+        return render(request, 'cart.html',{'products':products})
+
